9ab11d8d9636147401378632bbbd9d4586cb48f2
Kirren Covey <kirrencovey@gmail.com>
2019-02-14 13:34:04 -0600

    added button for adding news

--- src/scripts/profile/createNews.js
+++ src/scripts/profile/createNews.js
@@ -5,23 +5,24 @@ import newsHTML from "../profile/newsHTML"
 const createNews = () => {
     // Get news objects from API
     API.GET("users?_embed=friendships&_embed=news")
-    .then((userArray) => {
-        // Check IF userId on news objects matches active user
-        let activeUser = parseInt(document.querySelector("#userId").value)
-        userArray.forEach (user => {
-            if (activeUser === user.id) {
-                let userName = user.userName
-                let newsArray = user.news
-                // Turn each matching news object into HTML
-                newsArray.forEach(newsObject => {
-                    let newsItemHTML = newsHTML(newsObject, userName)
-                    // Print HTML news to DOM
-                    printToDom(newsItemHTML, "#news")
-                })
-            }
+        .then((userArray) => {
+            // Check IF userId on news objects matches active user
+            let activeUser = parseInt(document.querySelector("#userId").value)
+            userArray.forEach (user => {
+                if (activeUser === user.id) {
+                    let userName = user.userName
+                    let newsArray = user.news
+                    // Turn each matching news object into HTML
+                    newsArray.forEach(newsObject => {
+                        let newsItemHTML = newsHTML(newsObject, userName) + "<button id='addNews'>Add News Item</button>"
+                        // Print HTML news to DOM
+                        printToDom(newsItemHTML, "#news")
+                    })
+                }
+            })
         })
-    })
 }
+
 
 
 
@@ -30,13 +31,29 @@ export default createNews
 
 
 
-// Check IF userId on news objects matches friend of active user
-// let friendsArray = user.friendships
-// let friendIds = []
-// friendsArray.forEach(friend => {
-//     friendIds.push(friend.friendId)
-//     console.log(friendIds)
-// })
+
+//   // Check IF userId on news objects matches friend of active user
+//   let friendsArray = user.friendships
+//   let friendIds = []
+//   friendsArray.forEach(friend => {
+//       friendIds.push(friend.friendId)
+//       console.log(friendIds)
+//   })
+
+//   friendIds.forEach(friendId => {
+//       if (friendId === user.id || activeUser === user.id) {
+//           let userName = user.userName
+//           let newsArray = user.news
+//           // Turn each matching news object into HTML
+//           newsArray.forEach(newsObject => {
+//               let newsItemHTML = newsHTML(newsObject, userName)
+//               // Print HTML news to DOM
+//               printToDom(newsItemHTML, "#news")
+//           })
+//       }
+//   })
+
+
 // friendIds.forEach(friendId => {
 //     if (friendId === user.id) {
 //         let userName = user.userName
