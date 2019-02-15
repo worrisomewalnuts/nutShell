15be4dec47f520d3363c9a0400c59c6d2af01249
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-15 15:44:46 -0600

    chat displays messages in DB, need to fix user name and time stamp on displayed messages

---
+++ src/scripts/chat/messageList.js
@@ -0,0 +1,9 @@
+import API from "../utilities/apiManager"
+import messageHTML from "./messageHTML"
+
+function messageList() {
+    return API.GET("chatMessages")
+    .then((parsedMessages) => messageHTML(parsedMessages))
+}
+
+export default messageList
