bf44fefe8a1e4df7df0fd123d29a088edffe9327
Dominic.W.Kantrude <dominic.kantrude52@yahoo.com>
2019-02-15 15:07:39 -0600

    made register page work

--- src/scripts/login/makeLoginHTML.js
+++ src/scripts/login/makeLoginHTML.js
@@ -1,7 +1,23 @@
+import registerNewUser from "./registerNewUser"
+
 const makeLoginHtml = () => {
     let html = `
     <h1>Welcome to NutShell</h1>
-    <a>Login</a>
-    <a>Sign-up</a>
+    <section id="loginPageOptions">
+    <a href="javascript:;" id="login">Login</a>
+    <a href="javascript:;" id="signUp">Sign-up</a>
+    <section>
     `
+    document.querySelector(".output").innerHTML = html
+
+    document.querySelector("#loginPageOptions").addEventListener("click", function(){
+
+        if (event.target.id === "login"){
+            console.log("login")
+        }else if(event.target.id === "signUp"){
+            registerNewUser()
+        }
+    })
 }
+
+export default makeLoginHtml
---
+++ src/scripts/login/registerNewUser.js
@@ -0,0 +1,60 @@
+import API from "../utilities/apiManager"
+import makeProfileHTML from "../profile/makeProfileHTML"
+const registerNewUser = () => {
+    let html = `
+<h1>Sign Up</h1>
+<section id="signUpSection">
+<label>UserName: </label>
+<input id="userName"></input>
+<label>Password: </label>
+<input id="password"></input>
+<label>Email: </label>
+<input id="email"></input>
+<label>First Name: </label>
+<input id="firstName"></input>
+<label>Last Name: </label>
+<input id="lastName"></input>
+<button id="registerNewUser">Register</button>
+</section>
+`
+    document.querySelector(".output").innerHTML = html
+
+    document.querySelector("#registerNewUser").addEventListener("click", function () {
+       let newUser = createNewUser()
+
+    API.POST("users", newUser).then(newUser=>{
+        makeProfileHTML(newUser.id)
+    })
+
+
+    })
+}
+
+class User {
+    constructor(userName, password, email, firstName, lastName) {
+        this.userName = userName
+        this.password = password,
+            this.email = email,
+            this.firstName = firstName,
+            this.lastName = lastName
+    }
+}
+
+const createNewUser = () => {
+
+    const userName = document.querySelector("#userName").value
+    const password = document.querySelector("#password").value
+    const email = document.querySelector("#email").value
+    const firstName = document.querySelector("#firstName").value
+    const lastName = document.querySelector("#lastName").value
+    let user = new User(userName, password, email, firstName, lastName);
+    return user
+
+}
+
+
+
+
+
+
+export default registerNewUser
