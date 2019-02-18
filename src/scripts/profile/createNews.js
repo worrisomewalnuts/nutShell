f1f38583c080230c2563bae9764529446c734e81
Kirren Covey <kirrencovey@gmail.com>
2019-02-18 15:10:26 -0600

    removed unnecessary commented code

--- src/scripts/profile/createNews.js
+++ src/scripts/profile/createNews.js
@@ -63,37 +63,3 @@ const createNews = () => {
 
 
 export default createNews
-
-
-
-
-
-
-// // Check IF userId on news objects matches active user OR active user's friends
-// let activeUserId = parseInt(document.querySelector("#userId").value)
-// let idsToFind = []
-
-// idsToFind.push(activeUserId)
-
-// let userFriendships = 
-
-
-
-// userArray.forEach (user => {
-//     if (activeUserId === user.id) {
-//         let userName = user.userName
-//         let newsArray = user.news
-//         // Turn each matching news object into HTML
-//         newsArray.forEach(newsObject => {
-//             let newsItemHTML = newsHTML(newsObject, userName) + "<button id='addNews'>Add News Item</button>"
-//             // Print HTML news to DOM
-//             printToDom(newsItemHTML, "#newsListSection")
-//         })
-//     }
-// })
-
-
-
-
-
-// })
