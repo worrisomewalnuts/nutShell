31af687cffb4f5ddd5dbeaf1b1c4ff46992fd400
Prafullata Sonawane <prafullatass@gmail.com>
2019-02-18 09:17:53 -0600

    added EL on task Name

--- src/scripts/profile/taskList.js
+++ src/scripts/profile/taskList.js
@@ -2,16 +2,18 @@ import printToDom from "../utilities/printToDOM";
 
 //html string for eack task
 const listHtml = (task) =>
-    `<div id = "task--${task.id}"> ${task.taskName} ${task.completionDate}</div>
-    <input id = "box--${task.id}" type="checkbox" name="completed" value=false> completed?
-    <button id="edit--${task.id}">Edit Task</button>
-    <button id="del--${task.id}">Delete Task </button>
+    `<div id = "task--${task.id}"> <span class = "taskDate"> ${task.completionDate}</span>
+     <span class = "taskName">${task.taskName}</span> </div>
+    <input id = "box--${task.id}" type="checkbox" name="completed" value=false> Completed?
+    <button id = "edit--${task.id}">Edit Task</button>
+    <button id = "del--${task.id}">Delete Task </button>
     <hr/>
 `
 
 // make a task list and post it to DOM
 const taskList = (tasks) => {
-    let htmlStr = ""
+    let htmlStr = "<strong> Completion Date  Task Name  </strong>"
+    tasks.sort((a, b) => new Date(a.completionDate) - new Date(b.completionDate))
     tasks.forEach(task => {
         htmlStr += listHtml(task)
     });
