bd7cfd70750844592278475020f46290f2915506
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-19 10:48:28 -0600

    private chat initially working

--- src/scripts/privateMessages/createPrivateMessages.js
+++ src/scripts/privateMessages/createPrivateMessages.js
@@ -1,16 +1,20 @@
 import API from "../utilities/apiManager";
+import privateMessageHTML from "./privateMessageHTML";
+
 
 function createPrivateMessages(id) {
-    let friendId = id
+    let friendId = parseInt(id)
     let userId = parseInt(document.querySelector("#userId").value)
     return API.GET("privateMessages")
     .then((parsedPrivMessages)=> {
         let messagesInThisConversation = parsedPrivMessages.filter((message) => {
-            return ((message.senderId === userId && message.recipientId === friendId) || (message.senderId === friendId && message.recipientId === userId)
+            return ((message.senderId === userId && message.recipientId === friendId) || (message.senderId === friendId && message.recipientId === userId))
         })
         return messagesInThisConversation
     })
-    .then((messagesInThisConversation)=> console.log({messagesInThisConversation}))
+    .then((messagesInThisConversation)=> {
+        privateMessageHTML(messagesInThisConversation)
+    })
 }
 
 export default createPrivateMessages
