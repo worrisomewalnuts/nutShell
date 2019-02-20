742ba63cb22b2ace7ba13a4a8484a908ba2bbb6b
Kirren Covey <kirrencovey@gmail.com>
2019-02-20 11:15:14 -0600

    starting refactoring

--- src/scripts/profile/newsEventListener.js
+++ src/scripts/profile/newsEventListener.js
@@ -5,6 +5,8 @@ import createNews from "./createNews"
 import editNewsForm from "./editNewsForm"
 import { isEmpty, isProfanity } from "../utilities/dataValidation"
 
+const $ = document.querySelector.bind(document)
+
 const newsEventListener = () => {
     document.querySelector("#news").addEventListener("click", (event) => {
         let userId = document.querySelector("#userId").value
@@ -21,7 +23,7 @@ const newsEventListener = () => {
             let newsSynopsis = document.querySelector("#newsSynopsis").value
             let newsURL = document.querySelector("#newsURL").value
 
-            //run data validation and create new news info is passed
+            //run data validation and create new news if info is passed
             if (isEmpty(news) || isEmpty(newsSynopsis) || isEmpty(newsURL)) {
                 alert("One or more fields are empty")
             } else if (isProfanity(news) || isProfanity(newsSynopsis) || isProfanity(newsURL)) {
@@ -56,42 +58,74 @@ const newsEventListener = () => {
             }
 
             // EDIT button
-        } else if (event.target.id.startsWith("editNewsButton")) {
+        } else if (event.target.id.startsWith("editNewsButton") && buttonParent.classList.contains(`user--${userId}`)) {
+            // Find ID of news item to edit
+            let idToEdit = event.target.id.split("--")[1]
+            // Show edit form
+            let editFormHTML = editNewsForm(idToEdit)
+            printToDom(editFormHTML, "#newsFormSection")
 
-            // Check that news belongs to active user
-            if (buttonParent.classList.contains(`user--${userId}`)) {
-                // Find ID of news item to edit
-                let idToEdit = event.target.id.split("--")[1]
-                // Show edit form
-                let editFormHTML = editNewsForm(idToEdit)
-                printToDom(editFormHTML, "#newsFormSection")
+            // UPDATE button
+            document.querySelector("#updateNews").addEventListener("click", () => {
+                let news = document.querySelector("#editNewsTitle").value
+                let newsSynopsis = document.querySelector("#editNewsSynopsis").value
+                let newsURL = document.querySelector(`#newsLink--${idToEdit}`).href
 
-                // UPDATE button
-                document.querySelector("#updateNews").addEventListener("click", () => {
-                    let news = document.querySelector("#editNewsTitle").value
-                    let newsSynopsis = document.querySelector("#editNewsSynopsis").value
-                    let newsURL = document.querySelector(`#newsLink--${idToEdit}`).href
+                let updatedNewsObject = {
+                    news: news,
+                    newsSynopsis: newsSynopsis,
+                    newsURL: newsURL,
+                    userId: parseInt(userId),
+                    date: Date().split(" ").splice(0, 4).join(" ")
+                }
 
-                    let updatedNewsObject = {
-                        news: news,
-                        newsSynopsis: newsSynopsis,
-                        newsURL: newsURL,
-                        userId: parseInt(userId)
-                    }
+                let apiString = `news/${idToEdit}`
+                // API call
+                API.EDIT(apiString, updatedNewsObject)
+                    .then(response => {
+                        createNews()
+                    })
 
-                    let apiString = `news/${idToEdit}`
-                    // API call
-                    API.EDIT(apiString, updatedNewsObject)
-                        .then(response => {
-                            createNews()
-                        })
-
-                    // Remove edit form
-                    document.querySelector("#newsFormSection").innerHTML = ""
-                })
-            }
+                // Remove edit form
+                document.querySelector("#newsFormSection").innerHTML = ""
+            })
         }
     })
 }
+
+
+// Add News Button
+$("#addNews").addEventListener("click", (event) => {
+    let newsFormHTML = newsForm()
+    printToDom(newsFormHTML, "#newsFormSection")
+})
+
+// Submit News Button
+$("#submitNews").addEventListener("click", (event) => {
+    let news = document.querySelector("#newsTitle").value
+    let newsSynopsis = document.querySelector("#newsSynopsis").value
+    let newsURL = document.querySelector("#newsURL").value
+
+    //run data validation and create new news if info is passed
+    if (isEmpty(news) || isEmpty(newsSynopsis) || isEmpty(newsURL)) {
+        alert("One or more fields are empty")
+    } else if (isProfanity(news) || isProfanity(newsSynopsis) || isProfanity(newsURL)) {
+        alert("One or more fields contain profanity")
+    } else {
+        let newNewsObject = {
+            news: news,
+            newsSynopsis: newsSynopsis,
+            newsURL: newsURL,
+            userId: parseInt(userId),
+            date: Date().split(" ").splice(0, 4).join(" ")
+        }
+
+        API.POST("news", newNewsObject)
+            .then(createNews)
+    }
+})
+
+
+
 
 export default newsEventListener
